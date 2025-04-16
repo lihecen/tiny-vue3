cd20043f54d9e5c4979abdbfc8a22dfe1c948823
Lihecen <2837842053@qq.com>
2025-04-16 20:54:15 +0800

    实现Effect的Scheduler功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,6 @@
 class ReactiveEffect {
     private _fn: any;
-    constructor(fn) {
+    constructor(fn, public scheduler?) {
         this._fn = fn;
     }
     run() {
@@ -34,15 +34,19 @@ export function trigger(target, key) {
    let dep = depsMap.get(key);
    //for循环
    for(const effect of dep) {
-    effect.run();
+    if(effect.scheduler) {
+        effect.scheduler();
+    } else {
+        effect.run();
+    }
    }
 } 
 
 
 let activeEffect;
-export function effect(fn) {
+export function effect(fn, options: any = {}) {
     //调用fn
-    const _effect = new ReactiveEffect(fn);
+    const _effect = new ReactiveEffect(fn, options.scheduler);
     _effect.run();
     return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -27,4 +27,27 @@ describe("effect", () => {
         expect(foo).toBe(12);
         expect(r).toBe("foo");
     });
+    it("scheduler", () => {
+        //具体工作流程: 
+        //通过effect第二个参数给定一个scheduler的fn
+        //effect第一次执行的时候，还会执行fn
+        //当响应式对象set update 不会执行fn而是会执行 scheduler
+        //如果说当执行scheduler的时候，会再次执行fn
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({ foo: 1});
+        const runner = effect(() => {
+            dummy = obj.foo;
+        }, {scheduler});
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        obj.foo ++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        expect(dummy).toBe(1);
+        run();
+        expect(dummy).toBe(2);
+    })
 });
