38ca9715e21c8cfbcc257e00d4700bea5b93d94e
Lihecen <2837842053@qq.com>
2025-04-17 19:32:46 +0800

    实现并完善Effect的stop功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from "./shared";
 class ReactiveEffect {
     private _fn: any;
+    deps = [];
+    onStop?: () => void;
+    //当前状态
+    active = true;
     constructor(fn, public scheduler?) {
         this._fn = fn;
     }
@@ -7,6 +12,22 @@ class ReactiveEffect {
         activeEffect = this;
         return this._fn();
     }
+    stop() {
+       //外部用户多次调用effect的时候deps也只会清空一次
+       if(this.active) {
+          cleanupEffect(this);
+          if(this.onStop) {
+            this.onStop();
+          }
+          this.active = false;
+       }
+    }
+}
+
+function cleanupEffect(effect) {
+   effect.deps.forEach((dep: any) => {
+     dep.delete(effect);
+   });
 }
 //依赖收集
 //用 Map 数据结构，形成target -> key -> dep 查询方案
@@ -24,7 +45,11 @@ export function track(target, key) {
         dep = new Set();
         depsMap.set(key, dep);
     }
+    if(!activeEffect) {
+        return;
+    }
     dep.add(activeEffect);
+    activeEffect.deps.push(dep);
 }
 
 //触发依赖
@@ -47,7 +72,16 @@ let activeEffect;
 export function effect(fn, options: any = {}) {
     //调用fn
     const _effect = new ReactiveEffect(fn, options.scheduler);
+    //options
+    Object.assign(_effect, options);
+    //extend
+    extend(_effect, options);
     _effect.run();
-    return _effect.run.bind(_effect);
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
 }
 
+export function stop(runner) {
+    runner.effect.stop();
+}
