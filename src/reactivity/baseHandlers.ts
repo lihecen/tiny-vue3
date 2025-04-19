e7ea10542390a8488e291e81ef86db72eefb5bd9
Lihecen <2837842053@qq.com>
2025-04-19 11:17:50 +0800

    实现shallowReadonly功能

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,10 +1,11 @@
 import { track, trigger } from "./effect";
 import { reactive, ReactiveFlags, readonly } from "./reactive";
-import { isObject } from "./shared";
+import { extend, isObject } from "./shared";
 const get = createGetter();
 const set = createSetter();
 const readonlyGet = createGetter(true);
-function createGetter(isReadonly = false) {
+const shallowReadonlyGet = createGetter(true, true);
+function createGetter(isReadonly = false, shallow = false) {
     return function get(target, key) {
       //判断是不是is_reactive
       if(key === ReactiveFlags.IS_REACTIVE){
@@ -13,6 +14,9 @@ function createGetter(isReadonly = false) {
          return isReadonly;
       }
       const res = Reflect.get(target, key);
+      if(shallow) {
+         return res;
+      }
       //判断res是否为object
       if(isObject(res)) {
          return isReadonly ? readonly(res) : reactive(res);
@@ -41,3 +45,6 @@ export const readonlyHandlers = {
       return true;
    },
 };
+export const shallowReadonlyHandlers = extend({}, readonlyHandlers, {
+   get: shallowReadonlyGet,
+});
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,4 +1,4 @@
-import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
+import { mutableHandlers, readonlyHandlers, shallowReadonlyHandlers } from "./baseHandlers";
 export const enum ReactiveFlags {
     IS_REACTIVE = "__v_isReactive",
     IS_READONLY = "__v_isReadonly",
@@ -7,6 +7,9 @@ export function reactive(raw) {
     //生成一个Proxy代理对象
     //用于知道何时调用 get() 方法来收集依赖，何时调用 set() 方法来触发依赖
     return createActiveObject(raw, mutableHandlers);
+}
+export function shallowReadonly(raw) {
+    return createActiveObject(raw, shallowReadonlyHandlers);
 }
 export function readonly(raw) {
     return createActiveObject(raw, readonlyHandlers);
