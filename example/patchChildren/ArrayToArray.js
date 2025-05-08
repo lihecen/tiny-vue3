fcd4257c51cbc0fa158507b24208b8258caef44d
Lihecen <2837842053@qq.com>
2025-05-08 19:21:30 +0800

    重构更新element 的双端对比 diff 算法

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -16,24 +16,24 @@ import { ref, h } from "../../lib/guide-tiny-vue3.esm.js";
 //];
 
 //2: 右侧的对比
-//a (b c)
-//d e (b c)
+// a (b c)
+// d e (b c)
 //const prevChildren = [
 //  h("p", { key: "A" }, "A"),
 //  h("p", { key: "B" }, "B"),
 //  h("p", { key: "C" }, "C"),
 //];
 //const nextChildren = [
-//  h("P", { key: "D" }, "D"),
+//  h("p", { key: "D" }, "D"),
 //  h("p", { key: "E" }, "E"),
 //  h("p", { key: "B" }, "B"),
 //  h("p", { key: "C" }, "C"),
 //];
 
-//3: 新节点比旧节点长 --> 创建新的节点
+//3: 新的比旧的长 --创建
 //左侧
 // (a b)
-// (a b) c d
+// (a b) c
 //const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
 //const nextChildren = [
 //  h("p", { key: "A" }, "A"),
@@ -44,15 +44,16 @@ import { ref, h } from "../../lib/guide-tiny-vue3.esm.js";
 
 //右侧
 // (a b)
-// c (a b)
-//const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
-//const nextChildren = [
-//  h("p", { key: "C" }, "C"),
-//  h("p", { key: "A" }, "A"),
-//  h("p", { key: "B" }, "B"),
-//];
+// d c (a b)
+const prevChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
+const nextChildren = [
+  h("p", { key: "D" }, "D"),
+  h("p", { key: "C" }, "C"),
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+];
 
-//4: 旧节点比新节点长 --> 删除旧的节点
+//4: 旧的比新的长--删除旧节点
 //左侧
 // (a b) c
 // (a b)
@@ -64,7 +65,7 @@ import { ref, h } from "../../lib/guide-tiny-vue3.esm.js";
 //const nextChildren = [h("p", { key: "A" }, "A"), h("p", { key: "B" }, "B")];
 
 //右侧
-//a (b c)
+// a (b c)
 // (b c)
 //const prevChildren = [
 //  h("p", { key: "A" }, "A"),
